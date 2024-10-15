d7747b303d9283e0d824aa996306535d20140582
Holly <holibuksa@gmail.com>
2024-10-15 13:07:13 +0200

    oppgave1d

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -5,7 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// TODO
-		
+		GPSPoint pungt = new GPSPoint(1, 2.0, 3.0, 5.0);
+
+		System.out.println("høgde: " + pungt.getElevation());
+		pungt.setTime(2);
+		System.out.println(pungt.toString());
 	}
 
 }
